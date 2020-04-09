a08d6f120f1e87a001378f7c88033bf54045f828
EwertonBello <ewerton.bello1212@gmail.com>
2020-04-09 17:25:12 -0300

    teste2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const url = "https://sslecal2.forexprostools.com/?columns=exc_flags,exc_currency,exc_importance,exc_actual,exc_forecast,exc_previous&features=datepicker,timezone&countries=110,17,29,25,32,6,37,36,26,5,22,39,14,48,10,35,7,43,38,4,12,72&calType=week&timeZone=12&lang=12"
+const url = "http://sslecal2.forexprostools.com/?columns=exc_flags,exc_currency,exc_importance,exc_actual,exc_forecast,exc_previous&features=datepicker,timezone&countries=110,17,29,25,32,6,37,36,26,5,22,39,14,48,10,35,7,43,38,4,12,72&calType=week&timeZone=12&lang=12"
 
 async function teste() {
   const response = await fetch(url)
