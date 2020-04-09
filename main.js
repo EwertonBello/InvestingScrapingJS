66c6ec241235e548513c7fb067ca1e109105e454
EwertonBello <ewerton.bello1212@gmail.com>
2020-04-09 17:35:31 -0300

    mode no-cors

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
 const url = "https://sslecal2.forexprostools.com/?columns=exc_flags,exc_currency,exc_importance,exc_actual,exc_forecast,exc_previous&features=datepicker,timezone&countries=110,17,29,25,32,6,37,36,26,5,22,39,14,48,10,35,7,43,38,4,12,72&calType=week&timeZone=12&lang=12"
 
-let header = new Headers();
-header.append("Cookie", "geoC=BR");
-
-let requestOptions = {
-  method: 'GET',
-  headers: header,
-  redirect: 'follow'
-};
-
-fetch(url,requestOptions)
+fetch(url,{mode: 'no-cors'})
   .then(response => response.text())
   .then(html => {
   	const doc = new DOMParser().parseFromString(html, "text/html");
